969850452fe9733c85172277d4aa2eb40877df4b
Your Name <youremail@example.com>
2024-10-02 20:36:04 -0400

    Registered Student model in admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'enrollment_date')
